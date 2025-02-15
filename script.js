1fb17d0db8ae7c93c21a946a3fe2527565249dac
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-15 15:54:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your code here
+const images = document.querySelectorAll(".image");
+let draggedItem = null;
+
+images.forEach(image => {
+    image.addEventListener("dragstart", (e) => {
+        draggedItem = e.target;
+        e.target.classList.add("selected");
+    });
+ image.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    });
+
+ 
+    image.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        if (draggedItem !== e.target) {
+            let temp = document.createElement("div");
+            e.target.replaceWith(temp);
+            draggedItem.replaceWith(e.target);
+            temp.replaceWith(draggedItem);
+        }
+        draggedItem.classList.remove("dragging");
+        draggedItem = null;
+    });
+
+  
+    image.addEventListener("dragend", () => {
+        if (draggedItem) {
+            draggedItem.classList.remove("selected");
+        }
+    });
+});
